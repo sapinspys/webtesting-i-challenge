846086e222fcbcbf42862d308724a232e27693fd
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-08 17:19:36 -0400

    Created repair test case

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,5 +2,15 @@ const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
 
 describe('enhancer.js', () => {
-  
+  const item = {
+    name: "Iron Sword",
+    durability: 30,
+    enhancement: 0
+  }
+
+  describe('repair', () => {
+    it('should raise durability to 100', () => {
+      expect(repair(item).durability).toBe(100);
+    })
+  })
 })
